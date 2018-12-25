c91d7f5dde0e484fd25148f748c22489a2cd9aa3
MuskteerAthos <muskteerathos@gmail.com>
2018-12-25 14:40:51 +0800

    add callbyvalue

--- src/main/java/com/muskteer/java/MethodCallByValueExample.java
+++ src/main/java/com/muskteer/java/MethodCallByValueExample.java
@@ -1,7 +1,29 @@
 package com.muskteer.java;
 
+/**
+ * JAVA是值传递！
+ * 方法的传参本质上就是赋值操作=
+ *      如果是基本类型（包含包装类 自动拆箱）作为形参不改变实际参数
+ *          基本类型（byte short int long float double boolean char）
+ *      引用类型作为形参改变实际参数
+ *          引用类型（对象引用 数组引用）
+ */
 public class MethodCallByValueExample {
     public static void main(String[] args) {
+        Person person = new Person(19);
+        test(person, 20);
+    }
 
+    public static void test(Person person, int age){
+        System.out.println("小明去年"+person.age);
+        person.age=age;
+        System.out.println("小明今年"+person.age);
+    }
+
+    static class Person{
+        int age ;
+        public Person(int age){
+            this.age = age;
+        }
     }
 }
